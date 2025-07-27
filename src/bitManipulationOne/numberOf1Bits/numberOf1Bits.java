d5170463f3413fb3aa90a092ea3113a7fb00d0e0
samina <khatunsamina962@gmail.com>
2025-07-27 12:37:11 +0530

    .

--- src/bitManipulationOne/numberOf1Bits/numberOf1Bits.java
+++ src/bitManipulationOne/numberOf1Bits/numberOf1Bits.java
@@ -1,4 +1,17 @@
 package bitManipulationOne.numberOf1Bits;
 
 public class numberOf1Bits {
+    int getNumberOf1Bits(int num){
+        int count=0;
+        for(int i=0;i<32;i++){
+            if((num&(1<<i))!=0){
+                count++;
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        numberOf1Bits n = new numberOf1Bits();
+        System.out.println(n.getNumberOf1Bits(11));
+    }
 }
